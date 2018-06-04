684f1fa03e1e5c80780cd66ba9600766d1219cae
eubeco <eubeco@gmail.com>
2018-06-04 09:57:27 +0200

    Explicación del patrón.

--- Patrones_SW_Basicos/src/capitulo8_patrones_sw_ejemplos/EjemploCadenaResponsabilidad.java
+++ Patrones_SW_Basicos/src/capitulo8_patrones_sw_ejemplos/EjemploCadenaResponsabilidad.java
@@ -1,5 +1,11 @@
 package capitulo8_patrones_sw_ejemplos;
 
+/* CADENA DE RESPONSABILIDAD
+ * Este patrón de comportamiento evita acoplar el emisor de una petición a su receptor dando a más 
+ * de un objeto la posibilidad de responder a una petición. Para ello se encadenan los receptores
+ * y pasa la petición a través de la cadena hasta que es procesada por algún objeto.
+ */
+
 public class EjemploCadenaResponsabilidad {
 	
 		public static void main(String[] args)
